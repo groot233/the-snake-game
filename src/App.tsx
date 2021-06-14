e1ded120e1289cbb6a12354884942d9b8680b351
Wanting Zeng <wtzeng@ucdavis.edu>
2021-06-14 15:54:43 -0700

    create Lawn

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,32 @@
 import * as THREE from 'three'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import './App.css';
 
-function Box(props: JSX.IntrinsicElements['mesh']) {
+function Lawn(props: JSX.IntrinsicElements['mesh']) {
   // This reference will give us direct access to the mesh
   const mesh = useRef<THREE.Mesh>(null!)
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-
-  return (
+  return (<>
     <mesh
       {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      ref={mesh}>
+      <boxGeometry args={[10, 1, 10]} />
+      <meshStandardMaterial color='rgb(0,100,0)'/>
     </mesh>
+    <axesHelper args={[100]} />
+  </>
   )
 }
 
 export default function App() {
+  console.log(window.innerHeight);
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <div id="canvasContainer" style={{ width: window.innerWidth, height: window.innerHeight }}>
+      <Canvas camera={{ position: [5, 5, 5] }}>
+        <ambientLight intensity={0.5}/>
+        <pointLight position={[5, 5, 5]} />
+        <Lawn position={[0, -1, 0]} />
+      </Canvas>
+    </div>
   )
 }
