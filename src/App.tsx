907b100cc034df479f6c3978186ae27cd2d6e37c
Wanting Zeng <wtzeng@ucdavis.edu>
2021-06-14 16:30:48 -0700

    Added Snake

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,38 @@ import './App.css';
 
 function Lawn(props: JSX.IntrinsicElements['mesh']) {
   // This reference will give us direct access to the mesh
-  const mesh = useRef<THREE.Mesh>(null!)
+  const mesh = useRef<THREE.Mesh>(null!);
   return (<>
     <mesh
       {...props}
       ref={mesh}>
-      <boxGeometry args={[10, 1, 10]} />
-      <meshStandardMaterial color='rgb(0,100,0)'/>
+      <boxGeometry args={[10, 3, 10]} />
+      <meshToonMaterial color='#578a34' />
     </mesh>
     <axesHelper args={[100]} />
   </>
-  )
+  );
+}
+
+function Snake(props: JSX.IntrinsicElements['mesh']) {
+  const mesh = useRef<THREE.Mesh>(null!);
+  return (
+    <>
+      <mesh {...props} ref={mesh}>
+      <boxGeometry args={[1, 1, 1]}/>
+      <meshToonMaterial color='#5076f9'/>
+      </mesh>
+    </>);
 }
 
 export default function App() {
-  console.log(window.innerHeight);
   return (
     <div id="canvasContainer" style={{ width: window.innerWidth, height: window.innerHeight }}>
       <Canvas camera={{ position: [5, 5, 5] }}>
-        <ambientLight intensity={0.5}/>
-        <pointLight position={[5, 5, 5]} />
-        <Lawn position={[0, -1, 0]} />
+        <ambientLight args={["0x404040", 0.2]} />
+        <pointLight position={[3, 3, 0]} />
+        <Snake position={[0, 0 , 0]}/>
+        <Lawn position={[0, -2, 0]} />
       </Canvas>
     </div>
   )
